7d5dc57b38618b924d81d51a9ff61f966e6b5493
Emma <65368545+electricblues@users.noreply.github.com>
2023-12-03 00:50:17 +0000

    added keys and  basic styles

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
 const key = document.querySelector('audio[data-key="a"]');
 console.log(key);
+
+function play() {
+  alert("play");
+}
+
+const playButton = document.querySelector("#play");
+playButton.addEventListener("click", () => play(playButton));

f953d54d4d7e25815cf1a1ca71a543580fb4e97a
Emma <65368545+electricblues@users.noreply.github.com>
2023-12-03 02:05:24 +0000

    play functionality, fonts, favicon, revised styles

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
-const key = document.querySelector('audio[data-key="a"]');
-console.log(key);
+// given a key, plays a sound
+function playSoundByKey(key) {
+  const dataKey = `[data-key="${key.toLowerCase()}"]`;
+
+  const audio = document.querySelector(`audio${dataKey}`);
+  const tile = document.querySelector(`div.key${dataKey}`);
+
+  if (audio) {
+    audio.currentTime = 0;
+    audio.play();
+    tile.classList.add("active");
+    tile.style.transition = "";
+    setTimeout(() => {
+      tile.classList.remove("active");
+      tile.style.transition = "all 0.5s";
+    }, 200);
+  }
+}
+
+window.addEventListener("keydown", (e) => playSoundByKey(e.key));
+window.addEventListener("keyup");
 
 function play() {
   alert("play");
 }
-
-const playButton = document.querySelector("#play");
-playButton.addEventListener("click", () => play(playButton));

d05d436e63d427294f982870eae2a368d74749d0
Emma <65368545+electricblues@users.noreply.github.com>
2023-12-03 03:26:59 +0000

    added game loop

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
-// given a key, plays a sound
+const LIST_OF_KEYS = ["a", "s", "d", "f", "g"];
+
+//generates a random list of keys of a particular length
+function newRandomKeys(length) {
+  const newArray = [];
+  for (let i = 0; i < length; i++) {
+    let randomInteger = Math.floor(Math.random() * LIST_OF_KEYS.length);
+    newArray.push(LIST_OF_KEYS[randomInteger]);
+  }
+  return newArray;
+}
+
+// given a key, it plays the sound associated with that key
+// changes the keys appearance when pressed/playing
 function playSoundByKey(key) {
   const dataKey = `[data-key="${key.toLowerCase()}"]`;
 
@@ -17,9 +30,61 @@ function playSoundByKey(key) {
   }
 }
 
-window.addEventListener("keydown", (e) => playSoundByKey(e.key));
-window.addEventListener("keyup");
-
-function play() {
-  alert("play");
+//returns a promise that plays a list of keys once, at a particular speed, and then resolves
+function computerTurn(keysToPlay, speed = 500) {
+  return new Promise((resolve) => {
+    let keyIndex = 0;
+    const myInterval = setInterval(() => {
+      playSoundByKey(keysToPlay[keyIndex]);
+      keyIndex++;
+      if (keyIndex === keysToPlay.length) {
+        clearInterval(myInterval);
+        resolve();
+      }
+    }, speed);
+  });
 }
+
+// makes sure that the player presses the keys in the correct order.
+// resolves true/false
+function playerTurn(keysToPlay) {
+  return new Promise((resolve) => {
+    let keyIndex = 0;
+
+    function keyEventHandler(event) {
+      if (event.key === keysToPlay[keyIndex]) {
+        playSoundByKey(event.key);
+        keyIndex++;
+      } else {
+        window.removeEventListener("keydown", keyEventHandler);
+        resolve(false); // player made a mistake
+      }
+
+      if (keyIndex === keysToPlay.length) {
+        window.removeEventListener("keydown", keyEventHandler);
+        resolve(true); // player played all keys correctly
+      }
+    }
+
+    window.addEventListener("keydown", keyEventHandler);
+  });
+}
+
+async function play() {
+  // generate a list of keys
+  let freshList = newRandomKeys(3);
+  let shouldPlay = true;
+  // we make the computer play it
+  while (shouldPlay) {
+    await computerTurn(freshList);
+    shouldPlay = await playerTurn(freshList);
+    freshList = newRandomKeys(3);
+    // we make the player play it
+    // repeat forever
+  }
+}
+
+// attache play functionality to the play button
+const playButton = document.querySelector("#play");
+
+playButton.addEventListener("click", () => play(playButton));
